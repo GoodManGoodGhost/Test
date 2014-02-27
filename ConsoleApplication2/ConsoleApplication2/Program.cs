62dbfe312f9b8914fa997723e9774ea7960f5c25
GoodManGoodGhost <2602522476@qq.com>
2014-02-28 00:07:52 +0800

    fist commit

---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "SO2=$$PPM";
+
+            try
+            {
+                if (str is string)
+                {
+                    throw new Exception("fdsfds");
+                }
+            }
+            catch (System.Exception ex)
+            {
+            	
+            }
+
+            Console.WriteLine(str);
+
+            Console.ReadKey();
+        }
+    }
+}
